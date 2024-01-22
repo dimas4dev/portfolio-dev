4835210f58d6aac28edf23249af1042cf3af03b2
Dimas Mendoza <dimas.mendoza@3tcapital.co>
2024-01-21 21:54:04 -0500

    Add translations for AboutMe component

--- src/components/AboutMe.jsx
+++ src/components/AboutMe.jsx
@@ -1,12 +1,12 @@
+import Translate from "@/components/Translation"
+
 const AboutMe = () => {
     return (
         <section className="flex flex-col md:flex-row gap-8 justify-center items-center">
             <div className="[&>p]:mb-4 [&>p>strong]:text-yellow-400 [&>p>strong]:font-semibold text-pretty order-2 md:order-1">
-                <p>Me llamo Miguel Ángel pero mis amigos me llaman midu. Empecé en la programación con un Amstrad, tenía 10 años. Actualmente estoy <strong>liderando equipos de desarrollo en multinacionales</strong>.</p>
-
-                <p>Algunos de mis éxitos incluyen <strong>colaborar con Mozilla para el desarrollo de las primeras apps en su sistema FirefoxOS</strong>. Aunque hoy está desaparecido fue un gran avance en el mundo del desarrollo web.</p>
-
-                <p>Como creador de contenido, <strong>cuento con el canal de habla hispana más visto del mundo en la categoría de <em className="italic">Software & Game Development</em> en Twitch</strong>. Mi objetivo es mejorar la empleabilidad de la comunidad hispana y el acceso a contenido de calidad.</p>
+                <p>{<Translate translationKey={"aboutMeName"} />} {<span className="text-sky-700"><Translate translationKey={"webDeveloper"} /></span>}{<Translate translationKey="aboutMeTime" />}{<span className="text-sky-700"><Translate translationKey="aboutMeProposal" /></span>}</p>
+                <p>{<Translate translationKey="aboutMeProposal01" />} {<span className="text-sky-700"><Translate translationKey="aboutMeObjective02" /></span>} {<Translate translationKey="aboutMeObjective03" />} {<Translate translationKey="aboutMeObjective04" />}</p>
+                <p>{<Translate translationKey="aboutMeObjective05" />} <span className="text-sky-700">{<Translate translationKey="aboutMeObjective06"/>}</span> {<Translate translationKey="aboutMeObjective07"/>} {<span className="text-sky-700"><Translate translationKey="aboutMeObjective08" /></span>} {<Translate translationKey="aboutMeObjective09"/>}</p>
             </div>
             <img src="/dimasNormie.webp" className="rounded-full size-96 order-1 md:order-2" />
         </section>
